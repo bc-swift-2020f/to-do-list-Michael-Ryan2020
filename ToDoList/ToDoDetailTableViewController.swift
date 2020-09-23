54a8e449b1fafa3c22fc5541400db32c14d0f7d7
Michael Ryan <ryanabu@bc.edu>
2020-09-22 23:23:25 -0400

    Ch. 5.3

---
+++ ToDoList/ToDoDetailTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ToDoDetailTableViewController.swift
+//  ToDoList
+//
+//  Created by Michael Ryan on 9/22/20.
+//  Copyright © 2020 Michael Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    
+    @IBOutlet weak var noteView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+        
+    }
+    
+}
